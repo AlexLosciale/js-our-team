f634319a697bf6d798a5b605b7d3c3d7b602b1f7
AlexLosciale <losciale17@gmail.com>
2025-01-23 13:12:51 +0100

    add:card

--- main.js
+++ main.js
@@ -40,3 +40,19 @@ let cardsMembers = [
 }
 ];
 
+let cardsConteiner = document.getElementById("flex")
+
+for (let index = 0; index < cardsMembers.length; index++){
+
+    <div class="flex">
+        <figure>
+            <img height="96px" src="./img/female1.png" alt="">
+        </figure>
+        <div class="flex flex-column">
+            <h5>lorem ipsum1</h5>
+            <span>test@test.it</span>
+            <span>lorem ipsum dolor</span>
+        </div>
+    </div>
+    
+};
