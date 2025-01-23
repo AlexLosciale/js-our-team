bcdc35c263e800e150e79f02a7220ca711e6c6b1
AlexLosciale <losciale17@gmail.com>
2025-01-23 13:06:11 +0100

    add:cards

--- main.js
+++ main.js
@@ -1,2 +1,42 @@
 //Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
 //(trovate l’array del team all’interno della cartella in allegato)
+
+let cardsMembers = [
+{
+    nome: "lorem ipsum1",
+    email: "test@test.it",
+    descrizione: "lorem ipsum dolor",
+    immagine: "./img/female1.png",
+},
+{
+    nome: "lorem ipsum1",
+    email: "test@test.it",
+    descrizione: "lorem ipsum dolor",
+    immagine: "./img/female1.png",
+},
+{
+    nome: "lorem ipsum1",
+    email: "test@test.it",
+    descrizione: "lorem ipsum dolor",
+    immagine: "./img/female1.png",
+},
+{
+    nome: "lorem ipsum1",
+    email: "test@test.it",
+    descrizione: "lorem ipsum dolor",
+    immagine: "./img/female1.png",
+},
+{
+    nome: "lorem ipsum1",
+    email: "test@test.it",
+    descrizione: "lorem ipsum dolor",
+    immagine: "./img/female1.png",
+},
+{
+    nome: "lorem ipsum1",
+    email: "test@test.it",
+    descrizione: "lorem ipsum dolor",
+    immagine: "./img/female1.png",
+}
+];
+
